151588732588d90c768ed59928b6ef84fd2abcf4
Raghvendra Chauhan <raghvendra.s@intimetec.com>
2023-11-01 20:32:20 +0530

    Added Assigment 1

---
+++ Assignment 1/EmailCarbonFootprint/Controllers/CarbonFootPrintController.cs
@@ -0,0 +1,37 @@
+using EmailCarbonFootprint.Models.Request;
+using EmailCarbonFootprint.Models.Response;
+using EmailCarbonFootprint.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EmailCarbonFootprint.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarbonFootPrintController : ControllerBase
+    {
+        
+        private readonly ILogger<CarbonFootPrintController> _logger;
+        private readonly ICarbonFootPrintService _carbonFootPrintService;
+
+        public CarbonFootPrintController(ILogger<CarbonFootPrintController> logger, ICarbonFootPrintService carbonFootPrintService)
+        {
+            _logger = logger;
+            _carbonFootPrintService = carbonFootPrintService;
+        }
+
+        [HttpPost]
+        public ActionResult<EmailCarbonFootPrintInfo> GetCarbonFootPrintForEmail(EmailModel emailModel)
+        {
+            try
+            {
+                return Ok(_carbonFootPrintService.GetCarbonFootPrintForEmail(emailModel));
+            }
+            catch(Exception ex) 
+            {
+                _logger.LogError(ex.Message);
+                return Problem(statusCode: (int)HttpStatusCode.BadRequest, title: ex.Message, type: ((int)HttpStatusCode.BadRequest).ToString());
+            }
+        }
+    }
+}
---
+++ Assignment 1/EmailCarbonFootprint/Models/Response/EmailCarbonFootPrintInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace EmailCarbonFootprint.Models.Response
+{
+    public class EmailCarbonFootPrintInfo
+    {
+        public string? EmailId { get; set; }
+        public double CarbonFootPrintByLongEmails { get; set; }
+        public double CarbonFootPrintByStandardEmails { get; set; }
+        public double CarbonFootPrintBySpamEmails { get; set; }
+        public double TotalCarbonFootPrintByEmails { get; set; }
+
+        public EmailCarbonFootPrintInfo(double CarbonFootPrintByLongEmails, double CarbonFootPrintByStandardEmails, double CarbonFootPrintBySpamEmails)
+        {
+            this.CarbonFootPrintByLongEmails = CarbonFootPrintByLongEmails;
+            this.CarbonFootPrintByStandardEmails = CarbonFootPrintByStandardEmails;
+            this.CarbonFootPrintBySpamEmails = CarbonFootPrintBySpamEmails;
+            TotalCarbonFootPrintByEmails = CarbonFootPrintBySpamEmails + CarbonFootPrintByLongEmails + CarbonFootPrintByStandardEmails;
+        }
+
+    }
+}
